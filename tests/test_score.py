3d0c643e4a098f6d63f21cc3a1c6b3e67507e31e
J183332 <j183332@tafe.wa.edu.au>
2026-05-11 13:17:47 +0800

    fix(score): update test and play route for 2D board

--- tests/test_score.py
+++ tests/test_score.py
@@ -1,15 +1,19 @@
 import unittest
 import app as game_app
-from app import check_winner
+from app import check_winner, P1
+
 
 class TestScoreCounter(unittest.TestCase):
+    def setUp(self):
+        game_app.board = game_app.new_board()
+
     def test_x_wins(self):
-        game_app.board[0] = 'X'
-        game_app.board[1] = 'X'
-        game_app.board[2] = 'X'
+        game_app.board[0][0] = P1
+        game_app.board[0][1] = P1
+        game_app.board[0][2] = P1
         result = check_winner()
-        assert result == 'X'
+        self.assertEqual(result, P1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
